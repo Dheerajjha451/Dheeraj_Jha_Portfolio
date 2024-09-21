e0584b246f4727345b7aa6c533d670c414190c7d
dheerajjha451 <dheerajjha451@gmail.com>
2024-09-21 14:02:40 +0530

    improve

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
 export const metadata: Metadata = {
   title: "Dheeraj Jha - Full Stack Developer, UI/UX Designer, and Freelance Developer",
   description: "Portfolio website of Dheeraj Jha, showcasing skills and projects in full stack development, UI/UX design, and freelance work.",
@@ -26,13 +27,20 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Dheeraj Jha - Full Stack Developer, UI/UX Designer, and Freelance Developer",
     description: "Explore the portfolio of Dheeraj Jha, showcasing skills and projects in full stack development, UI/UX design, and freelance work.",
-    url: "https://www.dheeraj-jha.tech/", 
+    url: "https://www.dheeraj-jha.tech/",
     type: "website",
-    
+    siteName: "Dheeraj Jha's Portfolio",
+    locale: "en_US",
+    images: [
+      {
+        url: "../public/assets/Dheeraj.jpeg", 
+        width: 1200,
+        height: 630,
+        alt: "Dheeraj Jha - Full Stack Developer Portfolio",
+      },
+    ],
   },
 };
-
-
 
 export default function RootLayout({
   children,
@@ -41,19 +49,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-<head>
-<meta name="google-site-verification" content="he2XSqBKe7HJzey6IC51NRFS8wdQ8TAQpv26jOk8sGQ" />
-</head>
-    <body>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
-    </body>
-  </html>
+      <head>
+        <meta name="google-site-verification" content="he2XSqBKe7HJzey6IC51NRFS8wdQ8TAQpv26jOk8sGQ" />
+      </head>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
