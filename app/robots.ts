e3010dbc4dbb73bef556d9abf733c0fedcb92b4f
dheerajjha451 <dheerajjha451@gmail.com>
2025-01-21 11:56:05 +0530

    update

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,14 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from 'next';
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
-    sitemap: 'https://www.dheeraj-jha.tech/sitemap.ts',
-  }
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/private/',
+      },
+    ],
+    sitemap: 'https://www.dheeraj-jha.tech/sitemap.xml',
+  };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,24 +1,26 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from 'next';
+
 export default function sitemap(): MetadataRoute.Sitemap {
+  const currentDate = new Date().toISOString();
+
   return [
     {
       url: 'https://www.dheeraj-jha.tech',
-      lastModified: new Date(),
+      lastModified: currentDate,
       changeFrequency: 'yearly',
-      priority: 1,
+      priority: 1.0,
     },
     {
       url: 'https://www.dheeraj-jha.tech/ui',
-      lastModified: new Date(),
+      lastModified: currentDate,
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: 'https://www.dheeraj-jha.tech/blogs',
-      lastModified: new Date(),
+      lastModified: currentDate,
       changeFrequency: 'weekly',
       priority: 0.5,
     },
-  ]
+  ];
 }
